3690de8478f9830a3110bdb4b4c9ad2b9b5e97ec
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2016-02-17 21:56:24 +0100

    Fix crash Readd subscriber

--- src/octomap_raycast_capability.cpp
+++ src/octomap_raycast_capability.cpp
@@ -49,16 +49,13 @@ void move_group::OctomapRaycastCapability::initialize()
 
   node_handle_.param("octomap_min_distance_to_obstacle", octo_min_distance_ ,0.05);
 
-  //service_ = root_node_handle_.advertiseService(CLEAR_OCTOMAP_SERVICE_NAME, &OctomapAccessCapability::clearOctomap, this);
-
-  //octomap_full_pub_ = node_handle_.advertise<octomap_msgs::Octomap>("octomap", 1, false);
-
-  //vis_timer_ = node_handle_.createTimer(ros::Duration(1.0), &move_group::OctomapRaycastCapability::visTimerCallback, this, false);
   ros::AdvertiseServiceOptions ops=ros::AdvertiseServiceOptions::create<hector_nav_msgs::GetDistanceToObstacle>("get_distance_to_obstacle", boost::bind(&move_group::OctomapRaycastCapability::lookupServiceCallback, this,_1,_2),ros::VoidConstPtr(),&service_queue_);
   dist_lookup_srv_server_ = node_handle_.advertiseService(ops);
 
-
   service_thread_=boost::thread(boost::bind(&move_group::OctomapRaycastCapability::serviceThread,this));
+
+  m_markerPub = node_handle_.advertise<visualization_msgs::MarkerArray>("distance_to_obstacle_debug_markers", 1, false);
+
 
   tf_ = this->context_->planning_scene_monitor_->getTFClient();
 }
@@ -75,123 +72,117 @@ bool move_group::OctomapRaycastCapability::lookupServiceCallback(hector_nav_msgs
                                                                  hector_nav_msgs::GetDistanceToObstacle::Response &res )
 {
 
-    ROS_DEBUG("Octomap distance lookup service called");
-    tf::StampedTransform camera_transform;
+  ROS_DEBUG("Octomap distance lookup service called");
+  tf::StampedTransform camera_transform;
 
-    const std::string& target_frame = context_->planning_scene_monitor_->getPlanningScene()->getPlanningFrame();
+  const std::string target_frame = context_->planning_scene_monitor_->getPlanningScene()->getPlanningFrame();
 
-    try{
-        bool useOutliers = true;
-        tf_->waitForTransform(target_frame ,req.point.header.frame_id, req.point.header.stamp, ros::Duration(1.0));
-        tf_->lookupTransform(target_frame, req.point.header.frame_id, req.point.header.stamp, camera_transform);
-
-        const octomap::point3d origin = octomap::pointTfToOctomap(camera_transform.getOrigin());
-
-        tf::Point end_point = camera_transform * tf::Point(req.point.point.x, req.point.point.y, req.point.point.z);
-        tf::Vector3 direction = end_point - camera_transform.getOrigin();
-
-        const octomap::point3d directionOc = octomap::pointTfToOctomap(direction);
-        std::vector<octomap::point3d> endPoints;
-        std::vector<float> distances;
-        int n=3;
-        endPoints.resize(1);
-        std::vector<octomap::point3d> directions;
-        directions.push_back(directionOc);
-
-        planning_scene_monitor::LockedPlanningSceneRO ls (context_->planning_scene_monitor_);
-
-        // Below is inspired by
-        // https://github.com/ros-planning/moveit_core/blob/jade-devel/planning_scene/src/planning_scene.cpp#L850
-        // and quite hacky. There should be a better way to access the octomap (at least read-only)?
-        collision_detection::CollisionWorld::ObjectConstPtr map = ls.getPlanningSceneMonitor()->getPlanningScene()->getWorld()->getObject("<octomap>");
-        const shapes::OcTree* octree_shape = static_cast<const shapes::OcTree*>(map->shapes_[0].get());
-        const boost::shared_ptr<const octomap::OcTree> octree_ = octree_shape->octree;
-
-        if(octree_->castRay(origin,directions[0],endPoints[0],true,5.0)) {
-            distances.push_back(origin.distance(endPoints[0]));
-        }else{
-
-        }
-
-        if (distances.size()!=0) {
-            int count_outliers;
-            endPoints.resize(1+(n*2));
-            // @TODO Note: distances not used anymore, has to be refactored.
-            get_endpoints(origin, *octree_, distances[0], direction, directions, endPoints, n);
-            if (useOutliers) {
-                double distance_threshold=0.7;
-                count_outliers=0;
-                for (size_t i =1;i<endPoints.size();i++){
-                    ROS_DEBUG("distance to checkpoints %f",endPoints[0].distance(endPoints[i]));
-                    if(endPoints[0].distance(endPoints[i])>distance_threshold) {
-                        count_outliers++;
-                    }
-                }
-                ROS_DEBUG("corner case: number of outliners: %d ",count_outliers);
-            }
-
-            res.distance = distances[0];
-
-            res.end_point.header.frame_id = target_frame;
-            res.end_point.header.stamp = req.point.header.stamp;
-            res.end_point.point.x = endPoints[0].x();
-            res.end_point.point.y = endPoints[0].y();
-            res.end_point.point.z = endPoints[0].z();
-
-
-
-
-            if (res.distance < octo_min_distance_) {
-                res.distance = -1.0;
-                ROS_WARN("Octomap GetDistanceToObstacle got distance under min_Distance -> returning -1.0");
-            }
-
-            if (useOutliers) {
-                if (count_outliers>=n-1) {
-                    res.distance=-1.0;
-                    ROS_DEBUG("Octomap GetDistanceToObstacle Corner Case");
-                }
-            }
-        } else {
-            res.distance=-1.0;
-        }
-
-        ROS_DEBUG("Result: getDistanceObstacle_Octo: distance: %f",res.distance);
-        if (m_markerPub.getNumSubscribers() > 0){
-            visualization_msgs::MarkerArray marker_array;
-            visualization_msgs::Marker marker;
-            marker.header.stamp = req.point.header.stamp;
-            marker.header.frame_id = target_frame;
-            marker.type = visualization_msgs::Marker::LINE_LIST;
-            marker.action = visualization_msgs::Marker::ADD;
-            marker.color.r= 1.0;
-            marker.color.a = 1.0;
-            marker.scale.x = 0.02;
-            marker.ns ="";
-            marker.action = visualization_msgs::Marker::ADD;
-            marker.pose.orientation.w = 1.0;
-
-            std::vector<geometry_msgs::Point> point_vector;
-            for (size_t i = 0; i < endPoints.size(); ++i){
-                geometry_msgs::Point tmp = octomap::pointOctomapToMsg(origin);
-                point_vector.push_back(tmp);
-
-                tmp = octomap::pointOctomapToMsg(endPoints[i]);
-                point_vector.push_back(tmp);
-            }
-            marker.points=point_vector;
-            marker_array.markers.push_back(marker);
-            m_markerPub.publish(marker_array);
-        }
-        return true;
-
-    }
-    catch(tf::TransformException e)
-    {
-        ROS_ERROR("Transform failed in lookup distance service call: %s",e.what());
-    }
-
+  try{
+    tf_->waitForTransform(target_frame ,req.point.header.frame_id, req.point.header.stamp, ros::Duration(1.0));
+    tf_->lookupTransform(target_frame, req.point.header.frame_id, req.point.header.stamp, camera_transform);
+  }catch(tf::TransformException e){
+    ROS_ERROR("Transform failed in lookup distance service call: %s",e.what());
     return false;
+  }
+
+  bool useOutliers = true;
+
+
+  const octomap::point3d origin = octomap::pointTfToOctomap(camera_transform.getOrigin());
+
+  tf::Point end_point = camera_transform * tf::Point(req.point.point.x, req.point.point.y, req.point.point.z);
+  tf::Vector3 direction = end_point - camera_transform.getOrigin();
+
+  const octomap::point3d directionOc = octomap::pointTfToOctomap(direction);
+  std::vector<octomap::point3d> endPoints;
+  std::vector<float> distances;
+  int n=3;
+  endPoints.resize(1);
+  std::vector<octomap::point3d> directions;
+  directions.push_back(directionOc);
+
+  planning_scene_monitor::LockedPlanningSceneRO ls (context_->planning_scene_monitor_);
+
+  // Below is inspired by
+  // https://github.com/ros-planning/moveit_core/blob/jade-devel/planning_scene/src/planning_scene.cpp#L850
+  // and quite hacky. There should be a better way to access the octomap (at least read-only)?
+  collision_detection::CollisionWorld::ObjectConstPtr map = ls.getPlanningSceneMonitor()->getPlanningScene()->getWorld()->getObject("<octomap>");
+  const shapes::OcTree* octree_shape = static_cast<const shapes::OcTree*>(map->shapes_[0].get());
+  const boost::shared_ptr<const octomap::OcTree> octree_ = octree_shape->octree;
+
+  if(octree_->castRay(origin,directions[0],endPoints[0],true,5.0)) {
+    distances.push_back(origin.distance(endPoints[0]));
+  }
+
+  if (distances.size()!=0) {
+    int count_outliers;
+    endPoints.resize(1+(n*2));
+    get_endpoints(origin, *octree_, distances[0], direction, directions, endPoints, n);
+    if (useOutliers) {
+      double distance_threshold=0.7;
+      count_outliers=0;
+      for (size_t i =1;i<endPoints.size();i++){
+        ROS_DEBUG("distance to checkpoints %f",endPoints[0].distance(endPoints[i]));
+        if(endPoints[0].distance(endPoints[i])>distance_threshold) {
+          count_outliers++;
+        }
+      }
+      ROS_DEBUG("corner case: number of outliers: %d ",count_outliers);
+    }
+
+    res.distance = distances[0];
+
+    res.end_point.header.frame_id = target_frame;
+    res.end_point.header.stamp = req.point.header.stamp;
+    res.end_point.point.x = endPoints[0].x();
+    res.end_point.point.y = endPoints[0].y();
+    res.end_point.point.z = endPoints[0].z();
+
+    if (res.distance < octo_min_distance_) {
+      res.distance = -1.0;
+      ROS_WARN("Octomap GetDistanceToObstacle got distance under min_Distance -> returning -1.0");
+    }
+
+    if (useOutliers) {
+      if (count_outliers>=n-1) {
+        res.distance=-1.0;
+        ROS_DEBUG("Octomap GetDistanceToObstacle Corner Case");
+      }
+    }
+  } else {
+    res.distance=-1.0;
+  }
+
+  ROS_DEBUG("Result: getDistanceObstacle_Octo: distance: %f",res.distance);
+  if (m_markerPub.getNumSubscribers() > 0){
+    visualization_msgs::MarkerArray marker_array;
+    visualization_msgs::Marker marker;
+    marker.header.stamp = req.point.header.stamp;
+    marker.header.frame_id = target_frame;
+    marker.type = visualization_msgs::Marker::LINE_LIST;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.color.r= 1.0;
+    marker.color.a = 1.0;
+    marker.scale.x = 0.02;
+    marker.ns ="";
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.orientation.w = 1.0;
+
+    std::vector<geometry_msgs::Point> point_vector;
+    for (size_t i = 0; i < endPoints.size(); ++i){
+      geometry_msgs::Point tmp = octomap::pointOctomapToMsg(origin);
+      point_vector.push_back(tmp);
+
+      tmp = octomap::pointOctomapToMsg(endPoints[i]);
+      point_vector.push_back(tmp);
+    }
+    marker.points=point_vector;
+    marker_array.markers.push_back(marker);
+    m_markerPub.publish(marker_array);
+  }
+
+  return true;
+
 }
 
 void move_group::OctomapRaycastCapability::get_endpoints(const octomap::point3d& origin,
